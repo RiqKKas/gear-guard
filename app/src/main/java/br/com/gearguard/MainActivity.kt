d44246785d9b4eb40d487416e344d52873a03cb7
the-riquelme <riquelmedamiaosilva@gmail.com>
2023-11-04 18:08:13 -0300

    :bricks: ci: update initial project structure

---
+++ app/src/main/java/br/com/gearguard/MainActivity.kt
@@ -0,0 +1,10 @@
+package br.com.gearguard
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
